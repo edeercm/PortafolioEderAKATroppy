d73a8ba4d2997eb683ec8ce76811bfdde0010f72
Eder <132967872+edeercm@users.noreply.github.com>
2023-08-16 11:48:19 -0600

    Update

--- js/close-menu.js
+++ js/close-menu.js
@@ -1,8 +1,25 @@
-const menuItems = document.querySelectorAll('.menu-link');
-const menuCheckbox = document.getElementById('click');
+// const menuItems = document.querySelectorAll('.menu-link');
+// const menuCheckbox = document.getElementById('click');
 
-   menuItems.forEach(item => {
-      item.addEventListener('click', () => {
-         menuCheckbox.checked = false;
+//    menuItems.forEach(item => {
+//       item.addEventListener('click', () => {
+//          menuCheckbox.checked = false;
+//       });
+//    });
+
+
+   const menuCheckbox = document.getElementById('click');
+
+   menuCheckbox.addEventListener('change', () => {
+      if (!menuCheckbox.checked) {
+         return; // No hagas nada si el menú se está cerrando
+      }
+      
+      const menuItems = document.querySelectorAll('.menu-link');
+      
+      menuItems.forEach(item => {
+         item.addEventListener('click', () => {
+            menuCheckbox.checked = false; // Cierra el menú desplegable al seleccionar una opción
+         });
       });
    });
