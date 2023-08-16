401e0107dd80986122d249e848fd7c931072267c
Eder <132967872+edeercm@users.noreply.github.com>
2023-08-16 11:56:28 -0600

    Update

--- js/close-menu.js
+++ js/close-menu.js
@@ -8,18 +8,18 @@
 //    });
 
 
-   const menuCheckbox = document.getElementById('click');
+const menuItems = document.querySelectorAll('.menu-link');
+const menuCheckbox = document.getElementById('click');
 
-   menuCheckbox.addEventListener('change', () => {
-      if (!menuCheckbox.checked) {
-         return; // No hagas nada si el menú se está cerrando
-      }
-      
-      const menuItems = document.querySelectorAll('.menu-link');
-      
+   // Verificar si estamos en una versión móvil
+   function isMobile() {
+      return window.innerWidth <= 885; // Ajusta el valor según tus necesidades
+   }
+
+   if (isMobile()) {
       menuItems.forEach(item => {
          item.addEventListener('click', () => {
-            menuCheckbox.checked = false; // Cierra el menú desplegable al seleccionar una opción
+            menuCheckbox.checked = false; // Cierra el menú desplegable al seleccionar una opción en la versión móvil
          });
       });
-   });
+   }
