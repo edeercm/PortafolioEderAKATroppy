d6ee69cc946a46d6ad6c7c2676cb5ec302b45517
Eder <132967872+edeercm@users.noreply.github.com>
2023-08-16 23:08:11 -0600

    Update

--- js/close-menu.js
+++ js/close-menu.js
@@ -1,13 +1,3 @@
-// const menuItems = document.querySelectorAll('.menu-link');
-// const menuCheckbox = document.getElementById('click');
-
-//    menuItems.forEach(item => {
-//       item.addEventListener('click', () => {
-//          menuCheckbox.checked = false;
-//       });
-//    });
-
-
 const menuItems = document.querySelectorAll('.menu-link');
 const menuCheckbox = document.getElementById('click');
 
